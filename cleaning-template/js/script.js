fe31e2f8c2533e68dc84abe8b605c6c580a4f776
yazzans <120238240+yazzans@users.noreply.github.com>
2022-12-10 08:51:08 +0300

    Add files via upload

---
+++ cleaning-template/js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    AOS.init();
+    window.onscroll = function () { myFunction() };
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop + 200;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+
+    }
+
+
+});
